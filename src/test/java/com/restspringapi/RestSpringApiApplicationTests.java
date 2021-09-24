fcd4c48f284241dc8e88bfd2299e90d2d2f886d2
Priteshraj Bhangale <priteshraj.bhangale@morningstar.com>
2021-09-24 23:24:26 +0530

    [pritesh] Initial commit

---
+++ src/test/java/com/restspringapi/RestSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
