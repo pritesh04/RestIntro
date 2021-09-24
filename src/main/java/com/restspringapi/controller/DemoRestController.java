9869c0a4d82f30278d6aaf14b9a183a360979413
Priteshraj Bhangale <priteshraj.bhangale@morningstar.com>
2021-09-24 23:26:06 +0530

    [pritesh] Created Rest Api To display Hello Message

---
+++ src/main/java/com/restspringapi/controller/DemoRestController.java
@@ -0,0 +1,16 @@
+package com.restspringapi.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+	@RequestMapping(value = "/web", method = RequestMethod.GET)
+	public String rest() {
+		return "Hello from Bridgelabs";
+	}
+	
+}
