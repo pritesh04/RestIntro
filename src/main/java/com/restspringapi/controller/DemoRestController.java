fc0a8e66556f16d2f92023e5379f566a8be77561
Priteshraj Bhangale <priteshraj.bhangale@morningstar.com>
2021-09-27 12:03:45 +0530

    [pritesh] Added Post Mapping using Json

--- src/main/java/com/restspringapi/controller/DemoRestController.java
+++ src/main/java/com/restspringapi/controller/DemoRestController.java
@@ -1,13 +1,18 @@
 package com.restspringapi.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.restspringapi.model.User;
+
 @RestController
 public class DemoRestController {
-	@RequestMapping(value = "/web", method = get)
+	@RequestMapping(value = "/web", method = RequestMethod.GET)
 	public String rest() {
 		return "Hello from Bridgelabs";
 	}
@@ -20,5 +25,10 @@ public class DemoRestController {
 	@RequestMapping(value = "/webb/{name}")
 	public String restPathValue(@PathVariable String name) {
 		return "Hello from Bridgelabs " +name ;
+	}
+	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName();
 	}
 }
